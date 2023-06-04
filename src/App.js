57bf3f6316c2e0b0c964fe07391a9a93e7134380
Dung Tran <dungtade160532@fpt.edu.vn>
2023-06-04 20:31:02 +0700

    Presentational and Container Components

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Menu from './components/MenuComponent';
 import { DISHES } from './shared/dishes';
+import Main from './components/MainComponent';
 class App extends Component {
   constructor(props) {
     super(props);
@@ -15,12 +16,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-          </div>
-        </Navbar>
-        <Menu dishes={this.state.dishes} />
+       < Main />
       </div>
     );
   }
