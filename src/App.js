e1d7e752080c135b2b2b8a5de1bbafe1a239e6c1
Dung Tran <dungtade160532@fpt.edu.vn>
2023-06-17 16:21:19 +0700

    React Router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
 import { DISHES } from './shared/dishes';
 import Main from './components/MainComponent';
+import { BrowserRouter } from 'react-router-dom';
 class App extends Component {
   constructor(props) {
     super(props);
@@ -14,10 +13,11 @@ class App extends Component {
     };
   }
   render() {
-    return (
+    return (<BrowserRouter>
       <div className="App">
        < Main />
       </div>
+      </BrowserRouter>
     );
   }
 }
